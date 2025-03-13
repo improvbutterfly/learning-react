da4b53cc2b5cb2da9356c4d809e8f4c6713e5506
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2025-03-13 15:43:03 -0700

    Add beginning of form with Met API call for departments

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [departments, setDepartments] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/departments')
+      .then((response) => response.json())
+      .then((data) => setDepartments(data.departments))
+  }, [])
+
+  //console.log(departments)
+
+  
+  const departmentOptions = departments.map((department) => {
+    return (
+      <option key={department.departmentId} value={department.departmentId}>
+        {department.displayName}
+      </option>
+    )
+  })
+
+  //console.log(departmentOptions)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section id="searchBox">
+        <h1>Search the Met Museum Collection</h1>
+        <form name="filterSearch">
+          <label>Search:
+          <input type="text" name="search" placeholder="Search" /></label>
+          <label>Museum Department:
+          <select name="department" defaultValue="">
+            <option value="" disabled>Select a Department</option>
+            { departmentOptions }
+          </select></label>
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+      <section id="resultsWindow">
+
+      </section>
     </>
   )
 }
