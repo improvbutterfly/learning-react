9034faa67d3e31054a4adc25631770147865a67c
improvbutterfly <68628212+improvbutterfly@users.noreply.github.com>
2025-03-13 17:12:36 -0700

    Handle form and click functions basics

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -6,15 +6,39 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("")
   const [searchResults, setSearchResults] = useState([])
 
+  // Get the department information from the Met API and store it in the
+  // departments state
   useEffect(() => {
     fetch('https://collectionapi.metmuseum.org/public/collection/v1/departments')
       .then((response) => response.json())
       .then((data) => setDepartments(data.departments))
   }, [])
 
-  //console.log(departments)
+  // When the form is submitted, prevent the default form submission behavior
+  // and fetch the search results from the Met API
+  function handleSearchSubmit(event) {
+    event.preventDefault()
 
-  
+    const selectedDepartment = event.target.department.value
+    console.log(selectedDepartment)
+    const searchString = (
+      `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchTerm}`
+      + (selectedDepartment ? `&departmentId=${selectedDepartment}` : "")
+    )
+
+    fetch(searchString)
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data))
+
+    console.log(searchResults)
+  }
+
+  // When the search term changes, update the searchTerm state
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value)
+    //console.log(searchTerm)
+  }
+
   const departmentOptions = departments.map((department) => {
     return (
       <option key={department.departmentId} value={department.departmentId}>
@@ -29,15 +53,23 @@ function App() {
     <>
       <section id="searchBox">
         <h1>Search the Met Museum Collection</h1>
-        <form name="filterSearch">
+        <form name="filterSearch" onSubmit={handleSearchSubmit}>
           <label>Search:
-          <input type="text" name="search" placeholder="Search" /></label>
+          <input 
+            type="text" 
+            name="search" 
+            placeholder="Search" 
+            value={searchTerm}
+            onChange={handleSearchChange}
+          /></label>
           <label>Museum Department:
           <select name="department" defaultValue="">
             <option value="" disabled>Select a Department</option>
+            <option value="" disabled>--</option>
+            <option value="">None</option>
             { departmentOptions }
           </select></label>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit"/>
         </form>
       </section>
       <section id="resultsWindow">
